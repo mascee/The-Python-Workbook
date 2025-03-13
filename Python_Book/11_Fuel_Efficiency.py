7fa8b955d622b27899d32edde0405e198481b19b
Olga Petrova <mascee@gmail.com>
2025-03-12 17:43:47 -0700

    Added 11_Fuel_Efficiency.py

---
+++ Python_Book/11_Fuel_Efficiency.py
@@ -0,0 +1,9 @@
+#Convert MPG to L/100 km
+
+MPG = int(input("Please enter MPG of your car: "))
+print(f"MPG of your car is {MPG} MPG.")
+
+L_100km = MPG/ 235.21
+
+print(f"Your car's takes {L_100km:.2f} literes to drive 100 km. ")
+
