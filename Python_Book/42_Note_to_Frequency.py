2ab3560c559b8810626e3ac08c863a7d22fefff0
Olga Petrova <mascee@gmail.com>
2025-03-21 18:51:14 -0700

    Edited 42

--- Python_Book/42_Note_to_Frequency.py
+++ Python_Book/42_Note_to_Frequency.py
@@ -1,25 +1,23 @@
 # This program reads a music note from the user and determines its frequency.
 
-note = input("Please enter a music note in this format C4, D4, E4 etc: ")
+note = input("Please enter a music note (C4, D4, E4, etc.): ")
 
-#Notes frequencies
-
-frequencies = {
-    "C4": 261.63,
-    "D4": 293.66,
-    "E4": 329.63,
-    "F4": 349.23,
-    "G4": 392.00,
-    "A4": 440.00,
-    "B4": 493.88
+# Notes base frequencies for the 4th octave
+base_frequencies = {
+    "C": 261.63,
+    "D": 293.66,
+    "E": 329.63,
+    "F": 349.23,
+    "G": 392.00,
+    "A": 440.00,
+    "B": 493.88
 }
 
+pitch = note[0]       # Get the note
+octave = int(note[1]) # Get the octave number 
 
-if note in frequencies:
-    {
-        print(f"Frequency of a {note} is {frequencies[note]} Hz. ")
-    }
+if pitch in base_frequencies:
+    freq = base_frequencies[pitch] / (2 ** (4 - octave))  # Adjust frequency based on octave
+    print(f"Frequency of {note} is {freq:.2f} Hz.")
 else:
-    {
-        print("Note is not recognized. ")
-    }
+    print("Note is not recognized.")
---
+++ Python_Book/43_Frequency_to_Note.py
@@ -0,0 +1,12 @@
+# This program reads frequency and tells what music note this is.
+
+frequencies = {
+    "C4": 261.63,
+    "D4": 293.66,
+    "E4": 329.63,
+    "F4": 349.23,
+    "G4": 392.00,
+    "A4": 440.00,
+    "B4": 493.88
+}
+
