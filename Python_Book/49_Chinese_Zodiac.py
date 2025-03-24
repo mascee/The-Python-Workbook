78443f097693f902ea2666ef9d8425a6ebb2a494
Olga Petrova <mascee@gmail.com>
2025-03-24 14:55:42 -0700

    Added 49_Chinese_Zodiac.py

---
+++ Python_Book/49_Chinese_Zodiac.py
@@ -0,0 +1,32 @@
+# This program reads the year from the user and displays Chinese Zodiac sign.
+
+year = int(input("What year were you born? "))
+
+if year % 12 == 0:
+    animal = "Monkey"
+elif year %12 == 1:
+    animal = "Rooster"
+elif year % 12 == 2:
+    animal = "Dog"
+elif year % 12 == 3:
+    animal = "Pig"
+elif year % 12 == 4:
+    animal = "Rat"
+elif year % 12 == 5:
+    animal = "Ox"
+elif year % 12 == 6:
+    animal = "Tiger"
+elif year % 12 == 7:
+    animal = "Hare"
+elif year % 12 == 8:
+    animal = "Dragon"
+elif year % 12 == 9:
+    animal = "Snake"
+elif year % 12 == 10:
+    animal = "Horse"
+elif year % 12 == 11:
+    animal = "Sheep"
+else:
+    print("Error. ")
+
+print(f"{year} is the year of {animal}. ")
