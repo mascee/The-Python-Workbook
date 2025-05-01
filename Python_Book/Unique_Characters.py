ac5523ce7a655e16fe31a8af4ffcfbcf90169d0a
Olga Petrova <mascee@gmail.com>
2025-05-01 12:04:47 -0700

    142. Unique_Characters.py

---
+++ Python_Book/Unique_Characters.py
@@ -0,0 +1,14 @@
+# Exercise 142. Unique Characters.
+# Create a program that determines and displays the number of unique characters
+# in a string entered by the user.
+# Use a dictionary to solve this problem
+
+s = input("Enter a string: ")
+
+characters = {}
+for ch in s:
+    characters[ch] = True
+
+print(f"There are {len(characters)} unique characters in {s}. ")
+
+print(characters)
